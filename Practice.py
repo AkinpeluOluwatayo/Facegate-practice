288fd4f08ff1ff222ca9e82296e79bfba988873e
Akinpelu Oluwatayo <akinpeluoluwatayo1235@gmail.com>
2025-08-06 15:59:06 +0100

    upload file

---
+++ Practice.py
@@ -0,0 +1,57 @@
+students_count = 1000
+print(students_count)
+
+ratings = 4.99
+print(ratings)
+
+is_published = True
+print(is_published)
+
+course_name = "Python Programming"
+print(course_name)
+
+print(len(course_name)) 
+print(course_name[5])
+print(course_name[-1])
+print(course_name[0:3])
+print(course_name[0:])
+print(course_name[:3])
+print(course_name[:])
+
+course_name = "Python \nProgramming"
+print(course_name)
+
+first = 'Akinpelu'
+last = 'Oluwatayo'
+
+full_name = first + ' '  + last
+print(full_name)
+
+#methods:.upper,lower,find(index) 
+
+course_name = "Python Programming"
+print(course_name.upper())
+print(course_name.lower())
+print(course_name.find('h'))
+
+
+#arithmetics
+x = 10
+y = 3
+
+print(10 + 3)
+print(10 - 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+input
+
+
+
+
+
+
+
+
+
